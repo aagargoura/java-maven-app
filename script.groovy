51dde4f555baff0c18f91022aff55ad9624471e5
aagargoura <ahmed.gargoura@gmail.com>
2026-09-03 20:16:33 +0200

    Update Jenkins Groovy script

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t aagargoura/demo-app:jma-2.0 .'
+        sh 'docker build -t aagargoura/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push aagargoura/demo-app:jma-2.0'
+        sh 'docker push aagargoura/demo-app:jma-2.1'
     }
 }
 
