0de3028a66c3bb96aef29fb54d3748c81c6d9547
aagargoura <ahmed.gargoura@gmail.com>
2026-09-05 16:02:31 +0200

    call library functions in jenkins

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo 'Building the Jar...'
-    sh 'mvn clean package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t aagargoura/demo-app:jma-2.1 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push aagargoura/demo-app:jma-2.1'
-    }
-}
-
 def executeTests() {
     echo "Testing the Application"
     echo "Executing pipeline for branch $BRANCH_NAME"
